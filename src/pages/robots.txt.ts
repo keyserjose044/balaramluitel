d31ad74b756e3d76bf6ce09fc3ac4f8b4df4ea81
keyserjose044 <josemarquez044@gmail.com>
2025-11-26 22:54:57 -0600

    Update robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -5,7 +5,7 @@ export const GET: APIRoute = () => {
 User-agent: *
 Allow: /
 
-Sitemap: https://rohitashvasingh.com/sitemap.xml
+Sitemap: https://balaramluitel.com/sitemap.xml
 `;
 
   return new Response(content.trim(), {
